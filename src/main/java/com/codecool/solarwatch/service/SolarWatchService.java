227d10c3259c00fd3dd5f71a9e6bd762d3484b1d
doraerdelyi <163741439+doraerdelyi@users.noreply.github.com>
2025-02-27 11:24:14 +0100

    add dependencies to SolarWatchService

--- src/main/java/com/codecool/solarwatch/service/SolarWatchService.java
+++ src/main/java/com/codecool/solarwatch/service/SolarWatchService.java
@@ -1,6 +1,8 @@
 package com.codecool.solarwatch.service;
 
 import com.codecool.solarwatch.model.*;
+import com.codecool.solarwatch.repository.CityRepository;
+import com.codecool.solarwatch.repository.SunriseSunsetTimeRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -12,13 +14,18 @@ public class SolarWatchService {
     private static final String OPENWEATHER_API_URL = "http://api.openweathermap.org/geo/1.0/direct?q=%s&limit=%s&appid=%s";
     private static final String SUNRISE_SUNSET_API_URL = "https://api.sunrise-sunset.org/json?lat=%s&lng=%s&date=%s";
 
+    private CityRepository cityRepository;
+    private SunriseSunsetTimeRepository sunriseSunsetTimeRepository;
+
     @Value("${openweather.api.key}")
     private String OPENWEATHER_API_KEY;
 
     private final RestTemplate restTemplate;
 
-    public SolarWatchService(RestTemplate restTemplate) {
+    public SolarWatchService(RestTemplate restTemplate, CityRepository cityRepository, SunriseSunsetTimeRepository sunriseSunsetTimeRepository) {
         this.restTemplate = restTemplate;
+        this.cityRepository = cityRepository;
+        this.sunriseSunsetTimeRepository = sunriseSunsetTimeRepository;
     }
 
     public SunsetSunrise getSunsetAndSunriseByCity(String city, LocalDate date, String timezone) {
