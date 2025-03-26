d2032efe550308e0a323048953539757af9070de
doraerdelyi <163741439+doraerdelyi@users.noreply.github.com>
2025-03-26 15:37:12 +0100

    create AuthEntryPointJwt class that implements AuthenticationEntryPoint interface

--- src/main/java/com/codecool/solarwatch/security/WebSecurityConfig.java
+++ src/main/java/com/codecool/solarwatch/security/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.codecool.solarwatch.security;
 
+import com.codecool.solarwatch.security.jwt.AuthEntryPointJwt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
