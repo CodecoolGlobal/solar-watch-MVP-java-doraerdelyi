1dfae3e20a52972d1c21ddc72fbd92660d686674
doraerdelyi <163741439+doraerdelyi@users.noreply.github.com>
2025-03-26 14:13:22 +0100

    add UserController with register and login methods

---
+++ src/main/java/com/codecool/solarwatch/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.codecool.solarwatch.controller;
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+
+    @PostMapping("/register")
+    public UUID createUser(@RequestBody UserCreateDTO userCreateDTO) {
+        return UUID.randomUUID();
+    }
+
+    @PostMapping("/login")
+    public JwtResponseDTO login(@RequestBody UserLoginDTO userLoginDTO) {
+        return new JwtResponseDTO();
+    }
+}
