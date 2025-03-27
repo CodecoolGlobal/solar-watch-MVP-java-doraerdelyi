6c7ceaa4b18ded0c6974c4861cc94f85c3e71430
doraerdelyi <163741439+doraerdelyi@users.noreply.github.com>
2025-03-27 13:33:46 +0100

    create UserService class and implement login and register methods

---
+++ src/main/java/com/codecool/solarwatch/service/UserService.java
@@ -0,0 +1,57 @@
+package com.codecool.solarwatch.service;
+
+import com.codecool.solarwatch.DTO.JwtResponseDTO;
+import com.codecool.solarwatch.DTO.UserCreateDTO;
+import com.codecool.solarwatch.DTO.UserLoginDTO;
+import com.codecool.solarwatch.model.Role;
+import com.codecool.solarwatch.model.RoleType;
+import com.codecool.solarwatch.model.SunriseSunsetUser;
+import com.codecool.solarwatch.repository.SunriseSunsetUserRepository;
+import com.codecool.solarwatch.security.jwt.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class UserService {
+
+    private final SunriseSunsetUserRepository sunriseSunsetUserRepository;
+    private final RoleRepository roleRepository;
+    private final JwtUtils jwtUtils;
+    private final AuthenticationManager authenticationManager;
+    private final PasswordEncoder encoder;
+
+    @Autowired
+    public UserService(SunriseSunsetUserRepository sunriseSunsetUserRepository, RoleRepository roleRepository, JwtUtils jwtUtils, AuthenticationManager authenticationManager, PasswordEncoder encoder) {
+        this.sunriseSunsetUserRepository = sunriseSunsetUserRepository;
+        this.roleRepository = roleRepository;
+        this.jwtUtils = jwtUtils;
+        this.authenticationManager = authenticationManager;
+        this.encoder = encoder;
+    }
+
+
+    public void registerUser(UserCreateDTO userCreateDTO) {
+        Role role = this.roleRepository.findByRoleType(RoleType.ROLE_USER).get();
+        SunriseSunsetUser user = new SunriseSunsetUser(userCreateDTO.email(), userCreateDTO.password(), Set.of(role));
+        this.sunriseSunsetUserRepository.save(user);
+    }
+
+    public JwtResponseDTO loginUser(UserLoginDTO userLoginDTO) {
+        Authentication authentication = this.authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userLoginDTO.email(), userLoginDTO.password()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwtToken = jwtUtils.generateJwtToken(authentication);
+        User userDetails = (User) authentication.getPrincipal();
+        Set<String> roles = userDetails.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toSet());
+        return new JwtResponseDTO(jwtToken, userDetails.getUsername(), roles);
+    }
+}
