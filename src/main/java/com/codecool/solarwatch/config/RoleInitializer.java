a15448578568097f20e6465677f15a1676419bbc
doraerdelyi <163741439+doraerdelyi@users.noreply.github.com>
2025-03-29 17:13:48 +0100

    create component to initialize roles in db

---
+++ src/main/java/com/codecool/solarwatch/config/RoleInitializer.java
@@ -0,0 +1,33 @@
+package com.codecool.solarwatch.config;
+
+import com.codecool.solarwatch.model.Role;
+import com.codecool.solarwatch.model.RoleType;
+import com.codecool.solarwatch.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.Set;
+
+@Component
+public class RoleInitializer implements CommandLineRunner {
+
+    private final RoleRepository roleRepository;
+
+    public RoleInitializer(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    @Transactional
+    public void run(String... args) {
+        Set<RoleType> predefinedRoles = Set.of(RoleType.ROLE_USER, RoleType.ROLE_ADMIN);
+
+        for (RoleType roleType : predefinedRoles) {
+            roleRepository.findByRoleType(roleType)
+                    .orElseGet(() -> {
+                        Role role = new Role(roleType);
+                        return roleRepository.save(role);
+                    });
+        }
+    }
+}
--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO solar_watch.public.roles (role_type) VALUES ('ROLE_USER')
-ON CONFLICT (role_type) DO NOTHING;
-
-INSERT INTO solar_watch.public.roles (role_type) VALUES ('ROLE_ADMIN')
-ON CONFLICT (role_type) DO NOTHING;
