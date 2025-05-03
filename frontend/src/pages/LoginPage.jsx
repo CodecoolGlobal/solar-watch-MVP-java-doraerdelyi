ed64588692e18315c0afde5b244bb4e5495b5665
doraerdelyi <163741439+doraerdelyi@users.noreply.github.com>
2025-05-03 22:33:57 +0200

    reduce boilerplate code in LoginPage component

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,6 +1,8 @@
 import LoginForm from "../components/LoginForm.jsx";
 import {useState} from "react";
 import {useNavigate} from "react-router-dom";
+import ErrorAlert from "../components/ErrorAlert.jsx";
+import {postFetch} from "../Service/apiService.js";
 
 
 function LoginPage() {
@@ -11,13 +13,10 @@ function LoginPage() {
 async function handleLogin(credentials) {
     setError("");
     try {
-        const response = await fetch("/api/auth/login", {method: "POST", headers: {"Content-Type": "application/json"}, body: JSON.stringify(credentials)});
-        if (!response.ok) {
-            const message = await response.text();
-            setError(message || "Login failed");
-            return;
-        }
-        navigate("/sunrise-sunset");
+       const response = await postFetch("/api/auth/login", credentials);
+       const token = await response.json();
+       localStorage.setItem("token", token);
+       navigate("/sunrise-sunset");
     }
     catch(e) {
         setError(`Network error: ${e.message}`);
@@ -25,12 +24,7 @@ async function handleLogin(credentials) {
 
 }
     return <div><LoginForm onLogin={handleLogin}/>
-        {error && <div role="alert" className="alert alert-error">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>
-            <p>Error: {error}</p>
-        </div>}</div>
+        {error && <ErrorAlert errorMessage={error}/>}</div>
 }
 
 export default LoginPage;
